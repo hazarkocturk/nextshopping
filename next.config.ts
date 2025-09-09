2387135bae523553ca78512f9551246f30f2be73
hazar.kocturk <hazar.kocturk@iodigital.com>
2025-09-09 08:03:01 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    // Enable type checking during build time
-    ignoreBuildErrors: true, // Set to false in production
-  },
 };
 
 export default nextConfig;
